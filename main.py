2b3051778beb7600fda93ff9ddb59b42cac22d2a
arjunmanikandan <arjunmanikandan319@gmail.com>
2024-06-22 12:56:15 +0530

    Improvised the functions using lambda

--- main.py
+++ main.py
@@ -1,47 +1,39 @@
 import pandas as pd
-import numpy as np
-import json,sys
 
 def read_csv(csv_path):
     df = pd.read_csv(csv_path)
     return df
 
-def read_json(json_path):
-    with open(json_path,"r") as file:
-        data = json.load(file)
-    return data
+# Function to calculate costs
+def calculate_costs(df, wage):
+    df['total_cost'] = df.apply(lambda row: (wage / 60) * row['Time(mins)'] + row['Price(₹)'], axis=1)
+    return df
 
-def display_optimal_cost(df):
-    print(df)
-
-def calc_optimal_fares(transport_fares):
-    optimal_fares={
-    "min_cost" : [transport_fares.loc[0,"total_cost"]],
-    "max_cost" : [transport_fares.loc[1,"total_cost"]],
-    "min_company" :[ transport_fares.loc[0,"Company"]],
-    "max_company" : [transport_fares.loc[1,"Company"]]
+# Helper function to get min and max fares for each employee
+def get_min_max_fares(emp_row, transport_fares):
+    print
+    wage = emp_row['hourly_wage(₹)']
+    calculated_fares = calculate_costs(transport_fares.copy(), wage)
+    min_fare = calculated_fares.loc[calculated_fares['total_cost'].idxmin()]
+    max_fare = calculated_fares.loc[calculated_fares['total_cost'].idxmax()]
+    
+    return {
+        'min_total_cost': min_fare['total_cost'],
+        'min_company': min_fare['Company'],
+        'max_total_cost': max_fare['total_cost'],
+        'max_company': max_fare['Company']
     }
-    return pd.DataFrame(optimal_fares)
+# Apply calculations and find min/max costs using lambdas
+def get_fare_details(employee_wages, transport_fares):
+    fare_details_list = employee_wages.apply(lambda row: get_min_max_fares(row, transport_fares), axis=1)
+    fare_details_df = pd.DataFrame(fare_details_list.tolist(), index=employee_wages.index)
+    return pd.concat([employee_wages, fare_details_df], axis=1)
 
-def get_fare_details(employee_wages,transport_fares):
-    df_list=[]
-    for i in range(0,len(employee_wages.index)):
-        wage = employee_wages.loc[i]["hourly_wage(₹)"]
-        transport_fares["total_cost"] = transport_fares.apply(
-            lambda row:(wage/60)*row["Time(mins)"]+row["Price(₹)"],axis=1)
-        min_index = transport_fares["total_cost"].idxmin()
-        max_index = transport_fares["total_cost"].idxmax()
-        fare_details = transport_fares.loc[[min_index,max_index],["total_cost","Company"]].reset_index(drop=True)
-        optimal_fare = calc_optimal_fares(fare_details)
-        df_list.append(optimal_fare)
-    df = pd.concat(df_list, ignore_index=True)
-    return employee_wages.join(df)
 
-def main():
-    file_paths = read_json(sys.argv[1])
-    employee_wages = read_csv(file_paths["employee_wages_path"])
-    transport_fares = read_csv(file_paths["transport_fares_path"])
-    employee_wages_df = get_fare_details(employee_wages,transport_fares)
-    display_optimal_cost(employee_wages_df)
 
-main()
+# Calculate and display the fare details
+employee_wages = read_csv("employee_wages.csv")
+transport_fares = read_csv("transport_fares.csv")
+fare_details_df = get_fare_details(employee_wages, transport_fares)
+print(fare_details_df)
+
